a0d03b8770707817058f6b6e5429c46854794e4a
ZNANWANG <37197583+ZNANWANG@users.noreply.github.com>
2020-04-25 10:08:11 +0800

    init

---
+++ cmake-build-debug/CMakeFiles/C_Programming.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C_Programming.dir/main.c.obj"
+  "C_Programming.exe"
+  "C_Programming.exe.manifest"
+  "C_Programming.pdb"
+  "libC_Programming.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/C_Programming.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
